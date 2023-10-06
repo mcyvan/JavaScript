19280b13f38dec41f7e8c5ad1d86d31e05a8c70b
mcyvan <mcyvan@gmail.com>
2023-10-05 18:41:44 -0600

    practica 2

--- Practica_2/script.js
+++ Practica_2/script.js
@@ -1,1 +1,28 @@
-console.log("hola")
+var users=[{user:"Pedro", pass:"12345"},{user:"Fernanda", pass:"fer"},{user:"Francisco", pass:"1903"}
+];
+
+var user;
+var pass;
+var userR;
+var passR;
+
+function Extract(){
+    user=document.getElementById("user").value;
+    pass=document.getElementById("pass").value;
+    userR=document.getElementById("userR").value;
+    passR=document.getElementById("passR").value;
+}
+
+function LogIn(){
+    var s= false;
+    Extract();
+    for (i=0;i<users.length;i++){
+        if(user==users[i].user && pass==users[i].pass){
+            alert("Logueado");
+            s=true;
+        }
+    }
+    if(!s){
+        alert("Usuario o Contraseña Incorrecta");
+        }
+}
