661e3bf0a4252e27b984bdb05aa3ada3367d14ff
mcyvan <mcyvan@gmail.com>
2023-10-11 16:31:24 -0600

    termino de practica

--- Practica_2/script.js
+++ Practica_2/script.js
@@ -1,4 +1,6 @@
-var users=[{user:"Pedro", pass:"12345"},{user:"Fernanda", pass:"fer"},{user:"Francisco", pass:"1903"}
+var users=[{user:"Pedro", pass:"12345",type:"A"},
+           {user:"Fernanda", pass:"fer",type:"A"},
+           {user:"Francisco", pass:"1903",type:"B"}
 ];
 
 var user;
@@ -20,6 +22,9 @@ function LogIn(){
         if(user==users[i].user && pass==users[i].pass){
             alert("Logueado");
             s=true;
+            if(users[i].type=="A"){
+                ShowAdmin();
+            }
         }
     }
     if(!s){
@@ -29,6 +34,61 @@ function LogIn(){
 
 function Register(){
     Extract();
-    users.push({user:userR, pass:passR});
+    users.push({user:userR, pass:passR, type:"B"});
     alert("Agregado Correctamente");
 }
+
+function Show(){
+    var lista=document.getElementById("lista");
+    lista.innerHTML=" ";
+    for(let i=0;i<nuevoUsuario.length;i++){
+        // crear los elementos de la lista LI
+        var li=document.createElement("li");
+        // traer la informacion del arreglo al objeto li
+        li.innerHTML="Nombre: "+nuevoUsuario[i].user+" / Edad: "+ nuevoUsuario[i].age;
+        // 
+        lista.appendChild(li);
+    }
+}
+
+function ShowAdmin(){
+    var tabla=document.getElementById("tabla");
+    tabla.innerHTML="";
+    let tr1=document.createElement("tr");
+    let th1=document.createElement("th");
+    let th2=document.createElement("th");
+    th1.innerHTML="USUARIO";
+    th2.innerHTML="TIPO";
+    tr1.appendChild(th1);
+    tr1.appendChild(th2);
+    tabla.appendChild(tr1);
+
+    for(i=0;i<users.length;i++){
+        let tr =document.createElement("tr");
+        let td =document.createElement("td");
+        let td2=document.createElement("td");
+        let boton=document.createElement("button");
+        //llenamos los header con la informacion de la base de datos
+        boton.addEventListener("click",()=>{Splice(i)});
+        boton.innerHTML="Cambio";
+        // input.addEventListener("onchange",()=>{Splice(i, data)});
+        // se crean los hijos de las cabeceras
+        td.innerHTML=users[i].user;
+        td2.innerHTML=users[i].type;
+        tr.appendChild(boton);
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        tabla.appendChild(tr);
+    }
+}
+
+function Splice(D){
+    console.log(D);
+    if(users[D].type=="B"){
+        users[D].type="A";
+    }else{
+        users[D].type="B";
+    }
+    ShowAdmin();
+}
+
